3e19427f053772b226f4384a323a3204a1e6dfda
michelxd456 <michelbalvinor@gmail.com>
2023-02-22 16:43:16 -0500

    init

--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
-const inputCadena = document.getElementById("numeroDocumento");
-
-
+let inputCadena = document.getElementById("numeroDocumento");
  // Agregar un escucha de evento para cuando se escriba en la entrada
- inputCadena.addEventListener("input", () => {
-    // Obtener el valor de entrada sin espacios en blanco
-    const inputValue = inputCadena.value.replace(/\s/g, '');
+ inputCadena.addEventListener("input", (e) => {
+    let valor = e.target.value;
 
+    // Eliminar caracteres no numéricos y limitar el número de caracteres a 16
+    valor = valor.replace(/[^0-9]/g, '').substr(0, 16);
+    
+  
     // Dividir el valor de entrada en grupos de 4 caracteres
     const groups = [];
-    for (let i = 0; i < inputValue.length; i += 4) {
-      groups.push(inputValue.substr(i, 4));
+    for (let i = 0; i < valor.length; i += 4) {
+      groups.push(valor.substr(i, 4));
     }
-
+  
     // Unir los grupos con espacios
-    const outputValue = groups.join(' ');
-
-    // Establecer el valor de entrada con los espacios insertados
-    inputCadena.value = outputValue;
+    const valorSeparado = groups.join(' ');
+  
+    // Establecer el valor separado por espacios en el campo de entrada
+    e.target.value = valorSeparado;
   });
+  
+  
+let chec = document.getElementById("empresa");
+chec.addEventListener("change", function(){
+  
+  if(this.checked){
+    inputCadena.value = 4555;
+  }
+});
+let che = document.getElementById("persona");
+che.addEventListener("change",function(){
+  
+  inputCadena.value = ""
+  
+})
