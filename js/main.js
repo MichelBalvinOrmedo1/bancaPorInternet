d2c34c92f6f353ccfd83b370f881156e01470042
michelxd456 <michelbalvinor@gmail.com>
2023-02-24 13:38:58 -0500

    prueba

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-let inputCadena = document.getElementById("numeroDocumento");
- // Agregar un escucha de evento para cuando se escriba en la entrada
- inputCadena.addEventListener("input", (e) => {
-    //Ovtenemos el valor del nodo input
+
+
+let inputCadena = document.getElementById("numeroDocumento"); //obtenemos el elemento seleccionado como un objeto
+
+//Usamos el evento "input" para detectar cambios en los campos de entrada de texto, como los elementos input, textarea y select.
+inputCadena.addEventListener("input", (e) => { // (e) es el evento
+    //Obtenemos el valor actual del input
     let valor = e.target.value;
-    console.log(valor);
+    
     // Eliminar caracteres no numéricos y limitar el número de caracteres a 16
+    //valor.remplace() hace cuando tenemos numeros de 0 a 9 se remplaza en ''
+    //basicamente no ponemos escribir numeros de 0 a 9
+    //.substring() para limitar la longitud del valor de entrada a 16 caracteres.
     valor = valor.replace(/[^0-9]/g, '').substring(0,16)
     
   
     // Dividir el valor de entrada en grupos de 4 caracteres
     const groups = [];
+    //Agregamos grupos de 4
+    //divide el valor de entrada en grupos de cuatro caracteres utilizando un bucle for y el método substring() de JavaScript. 
+    //Cada grupo de cuatro caracteres se agrega a un array llamado groups utilizando el método push().
+
     for (let i = 0; i < valor.length; i += 4) {
-      groups.push(valor.substring(i, 4));
+      groups.push(valor.substring(i, i+4));
       
     }
   
@@ -23,6 +33,7 @@ let inputCadena = document.getElementById("numeroDocumento");
   });
   
   
+
 let chec = document.getElementById("empresa");
 chec.addEventListener("change", function(){
   
